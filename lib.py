edcd9be72977f116a8aad5b42817d3599936213e
Лимонова Мария <Marusia.limonova@gmail.com>
2022-11-01 18:53:03 +0500

    N_LISTS

--- lib.py
+++ lib.py
@@ -1,4 +1,25 @@
-import numpy as np
-a = [[0,1,1,1,5],[6,34,6,3,2,],[7,5,3,1,2,4,4]]
-b = a[0].count(1)
+n = int(input("n="))
+a = []
+c =[]
+for i in range (0,n):
+    print("Введите список")
+
+    count = int(input("count="))
+    c.append(count)
+    l = []
+    for j in range (0,count):
+        k=input()
+        l.append(k)
+    a.append(l)
+b = []
+for i in range (0,n):
+    max = -1
+    for j in range (0,c[i]):
+        if (max<a[i].count(a[i][j])):
+            max = a[i].count(a[i][j])
+
+    b.append(max)
 print(b)
+
+
+
